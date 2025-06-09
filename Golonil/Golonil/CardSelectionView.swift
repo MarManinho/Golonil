7360e8a4f0ed4f5c34f87e0b69f43eb6ac32ea11
Mukhtaram <mukhtaramsulaimonov@gmail.com>
2025-06-09 12:41:09 +0200

    new update

---
+++ Golonil/Golonil/CardSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CardSelectionView.swift
+//  Golonil
+//
+//  Created by Mukhtaram Sulaimonov on 03/06/25.
+//
+
+import SwiftUI
+
+struct CardSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardSelectionView()
+}
---
+++ Golonil/Golonil/GameExplanationView.swift
@@ -0,0 +1,18 @@
+//
+//  GameExplanationView.swift
+//  Golonil
+//
+//  Created by Mukhtaram Sulaimonov on 08/06/25.
+//
+
+import SwiftUI
+
+struct GameExplanationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameExplanationView()
+}
